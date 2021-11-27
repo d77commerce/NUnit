9364441d83bd468f1288a6d1e04590677a6f3876
Danail Tsachev <73217857+d77commerce@users.noreply.github.com>
2021-11-27 11:44:41 +0000

    new

--- NunitExers/Database.Tests/DatabaseTests.cs
+++ NunitExers/Database.Tests/DatabaseTests.cs
@@ -1,19 +1,50 @@
 using NUnit.Framework;
 
-namespace Tests
+
+namespace Database.Tests
 {
     [TestFixture]
     public class DatabaseTests
     {
-       [Test]
+        [SetUp]
         public void Setup()
         {
         }
 
         [Test]
-        public void Database()
+        public void Add_AddElementANextFreeCell_ReturnTrue()
         {
-            Assert.Pass();
+            Database database = new Database();
+            database.Add(1);
+            Assert.That(database.Count, Is.EqualTo(1));
+        }
+        [Test]
+        public void Add_MoreOf16Elements_Returnfalse()
+        {
+            Database database = new Database();
+
+            for (int i = 0; i < 16; i++)
+            {
+                database.Add(3);
+            }
+            Assert.That(() => database.Add(3), Throws.InvalidOperationException);
+        }
+        [Test]
+        public void Remuve_RemuveElementANextFreeCell_ReturnTrue()
+        {
+            Database database = new Database();
+            database.Add(1);
+            database.Add(2);
+            database.Remove();
+            var result = database.Count;
+            Assert.That(result, Is.EqualTo(1));
+        }
+        [Test]
+        public void Remuve_RemuveInEmptyDatabase_ReturnFalse()
+        {
+            Database database = new Database();
+
+            Assert.That(() => database.Remove(), Throws.InvalidOperationException);
         }
     }
 }
